c130eabef41222957c61f4b73002b219b4df2992
Flowery <45811382+Pink-Smile@users.noreply.github.com>
2021-01-19 16:42:30 +0800

    最新疫情自动上报

--- new_autoreport.py
+++ new_autoreport.py
@@ -3,7 +3,7 @@ import json
 import requests
 import urllib3
 
-url = "https://eai.buct.edu.cn/xisuncov/wap/open-report/save"
+url = "https://eai.buct.edu.cn/ncov/wap/default/save"
 
 if __name__ == '__main__':
 
@@ -15,55 +15,91 @@ if __name__ == '__main__':
     # report
     cookies = {}
     data = {
-        'sfzx': '1',  # 是否在校
-        'tw': '2',  # 体温范围（下标从 1 开始），此处是36 - 36.5
-        'area': '北京市 昌平区',  # 所在区域
-        'province': '北京市',  # 所在省
-        'city': '北京市',  # 所在市
-        'address': '北京市昌平区南口镇南涧路29号北京化工大学昌平校区',  # 地址
-        'sfcyglq': '0',  # 是否处于隔离期
-        'sfyzz': '0',  # 是否有症状
-        'askforleave': '0',  # 是否请假外出
-        'qtqk': '',  #其他情况
+        'ismoved': '0',  # 是否在校
+        'jhfjrq': '',
+        'jhfjjtgj': '',
+        'jhfjhbcc': '',
+        'sfxk': '0',
+        'xkqq': '',
+        'szgj': '',
+        'szcs': '',
+        'zgfxdq': '0',
+        'mjry': '0',
+        'csmjry': '0',
+        'tw': '3',  # 体温范围（下标从 1 开始），此处是36.6 - 36.9
+        'sfcxtz': '0',
+        'sfjcbh': '0',
+        'sfcxzysx': '',
+        'qksm': '',
+        'sfyyjc': '0',
+        'jcjgqr': '0',
+        'remark': '',
+        'address': '',
         'geo_api_info': {
-            'type': 'complete',
-            'info': 'SUCCESS',
-            'status': 1,
-            'Eia': 'jsonp_913580_',
-            'position': {
-                'O': 116.231204,  # 经度
-                'P': 40.22066,  # 纬度
-                'lng': 116.231204,  # 经度
-                'lat': 40.22066  # 纬度
-            },
-            'message': 'Get+ipLocation+success.Get+address+success.',
-            'location_type': 'ip',
-            'accuracy': None,
-            'isConverted': True,
-            'addressComponent': {
-                'citycode': '',
-                'adcode': '',  # 行政区划代码
-                'businessAreas': [],
-                'neighborhoodType': '',
-                'neighborhood': '',
-                'building': '',
-                'buildingType': '',
-                'street': '',
-                'streetNumber': '',
-                'province': '',  # 所在省
-                'city': '',  # 所在市
-                'district': '',  # 所在区
-                'township': ''  # 所在街道
-            },
-            'formattedAddress': '',  # 拼接后的地址
-            'roads': [],
-            'crosses': [],
-            'pois': []
+            "type": "complete", 
+            "info": "SUCCESS", 
+            "status": 1, 
+            "$Da": "jsonp_546186_", 
+            "position": {   # 经纬度，根据自己定位填
+                "Q": 32.25443, 
+                "R": 111.63742000000002, 
+                "lng": 111.63742, 
+                "lat": 32.25443
+            }, 
+            "message": "Get ipLocation success.Get address success.", 
+            "location_type": "ip", 
+            "accuracy": None, 
+            "isConverted": True, 
+            "addressComponent": {
+                "citycode": "0710", 
+                "adcode": "420625", 
+                "businessAreas": [], 
+                "neighborhoodType": "", 
+                "neighborhood": "", 
+                "building": "", 
+                "buildingType": "", 
+                "street": "", 
+                "streetNumber": "", 
+                "country": "", 
+                "province": "", 
+                "city": "", 
+                "district": "", 
+                "township": ""
+            }, 
+            "formattedAddress": "", 
+            "roads": [], 
+            "crosses": [], 
+            "pois": []
         },
+        'area': '',  # 手机定位地址
+        'province': '',    # 所在省
+        'city': '',   # 所在市
+        'sfzx': '0',
+        'sfjcwhry': '0',
+        'sfjchbry': '0',
+        'sfcyglq': '0',  # 是否处于隔离期
+        'gllx': '',
+        'glksrq': '',
+        'jcbhlx': '',
+        'jcbhrq': '',
+        'bztcyy': '',
+        'sftjhb': '0',
+        'sftjwh': '0',
+        'sfsfbh': '0',
+        'xjzd': '',
+        'jcwhryfs': '',
+        'jchbryfs': '',
+        'szsqsfybl': '0',
+        'sfygtjzzfj': '',
+        'gtjzzfjsj': '',
+        'gtjzzchdfh': '',
+        'fjqszgjdq': '',
+        'jcjg': '',
     }
-
 
     name = ''
     cookies['eai-sess'] = ''
-    result = s.post(url, data=data, headers=headers, cookies=cookies, verify=False)
+    cookies['UUkey'] = ''
+    result = s.post(url, data=data, headers=headers,
+                    cookies=cookies, verify=False)
     print(name + ':' + json.loads(result.text)['m'])
